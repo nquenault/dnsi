3cdfbcc39d9aaeab2b530af615f9d25984e1db64
Nicolas Quenault <nquenault@gmail.com>
2016-09-08 09:48:33 +0200

    Update NETExtensions.Part.vb

--- apps/Regex.Net/NETExtensions.Part.vb
+++ apps/Regex.Net/NETExtensions.Part.vb
@@ -63,7 +63,7 @@ Module NETExtensions
 				dim webclient as new system.net.webclient()
 				dim result as string = webclient.DownloadString(uri)
 				
-				msgbox(result)
+				msgbox(result.Length)
 
 				try
 					Dim stream as New IO.StreamWriter(New IO.MemoryStream(System.Text.Encoding.Default.GetBytes(result)))
