70b4fdd50b036f79327cb30c43336cf05697738a
Nicolas Quenault <nquenault@gmail.com>
2017-03-25 13:52:19 +0100

    Update Utility.cs

--- libraries/Utility/Utility.cs
+++ libraries/Utility/Utility.cs
@@ -28,8 +28,15 @@ static class Utility
 	{
 		private static readonly Random _random = new Random();
 		public static int Rand(int max = 100, int min = 0) { return _random.Next(min, max + 1); }
+		public static double Rand(double max = 100, double min = 0, bool round = true) { double v = _random.NextDouble() * (max - min) + min; return round ? Math.Round(v) : v; }
+		public static bool RandPourcentage(double pourcentage) {
+			double p = Rand((double)100);
+			return pourcentage >= p;
+		}
 	}
 	public static int Rand(int max = 100, int min = 0) { return Generator.Rand(max, min); }
+	public static double Rand(double max = 100, double min = 0, bool round = true) { return Generator.Rand(max, min, round); }
+	public static bool RandPourcentage(double pourcentage) { return Generator.RandPourcentage(pourcentage); }
 	public static T Rand<T>(IEnumerable<T> items)
 	{
 		return items.ElementAt(Utility.Rand(items.Count() - 1, 0));
